6777a387ea71bbfd1e2876a213922ed7a0c1cec3
liquidised <lincolnjolley787@gmail.com>
2023-02-06 13:41:34 -0600

    psuh

---
+++ starting-point/src/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Todo from './Todo'
+
+export default function TodoList({ todos }) {
+  return (
+    <div>
+        {todos.map(todo => {
+            return <Todo key={todo.id} todo={todo} />
+        })}
+    </div>
+  )
+}
