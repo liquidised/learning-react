435603c7560369a521c3795bfa63702c386b012d
liquidised <lincolnjolley787@gmail.com>
2023-02-06 13:41:31 -0600

    somethings broken

--- starting-point/src/App.js
+++ starting-point/src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
+import TodoList from "./TodoList";
+import React, { useState, useRef } from "react";
 
-function App() { 
+function App() {
+  const todoNameRef = useRef()
+  const [todos, setTodos] = useState([]);
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    console.log(name)
+  }
   return (
-    <TodoList />
-  )
+    <>
+      <TodoList todos={todos} />
+      <input ref="todoNameRef" type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button>Clear Completed Todos</button>
+      <div>0 left to do</div>
+    </>
+  );
 }
 
 export default App;
