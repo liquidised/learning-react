e8c7b948471facd938dc3cdd593139b325715863
liquidised <lincolnjolley787@gmail.com>
2023-02-06 14:08:58 -0600

    it doesn't really work but ok

--- starting-point/src/App.js
+++ starting-point/src/App.js
@@ -1,20 +1,41 @@
-import "./App.css";
 import TodoList from "./TodoList";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
+
 
 function App() {
   const todoNameRef = useRef()
   const [todos, setTodos] = useState([]);
+  const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem
+      (LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  function toggletodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
 
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
     if (name === '') return
-    console.log(name)
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: Math.random(), name: name, complete: false }]
+    })
+    todoNameRef.current.value = null
   }
   return (
     <>
       <TodoList todos={todos} />
-      <input ref="todoNameRef" type="text" />
+      <input type="text" ref={todoNameRef} />
       <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear Completed Todos</button>
       <div>0 left to do</div>
